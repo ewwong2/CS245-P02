43e5bf452ed0f541b50b3148e388ff5055fdbbe7
Noah King <nsking@dons.usfca.edu>
2017-09-28 10:58:42 -0700

    Graded and commented

--- BinaryRecursiveSearch.java
+++ BinaryRecursiveSearch.java
@@ -1,4 +1,4 @@
-package edu.usfca.cs.cs245;
+//package edu.usfca.cs.cs245;
 
 public class BinaryRecursiveSearch implements Practice2Search {
 
@@ -11,7 +11,11 @@ public class BinaryRecursiveSearch implements Practice2Search {
 	public int search(int[] arr, int target) {
 		return searchh(0,arr.length-1,arr,target);
 	}
-	
+
+	/*
+	There's no need to have different function names.  It actually can be more confusing to have
+	overloaded funcitons with different names.
+	*/	
 	private int searchh(int min, int max, int[] arr, int target) {
 		int guess = (min+max)/2;
 		if (min>max) {
